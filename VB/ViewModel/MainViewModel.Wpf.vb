af3bb3d609e0bedb5a033702e087a2b62501d6e8
DevExpressExampleBot <fakeEmail>
2022-12-17 14:03:16 +0400

    Source auto update [skip ci]

--- VB/ViewModel/MainViewModel.Wpf.vb
+++ VB/ViewModel/MainViewModel.Wpf.vb
@@ -1,82 +1,97 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports System.Windows
 
 Namespace Example.ViewModel
+
     Public Class MainViewModel
         Inherits ViewModelBase
 
-        Private privateAutoUpdateCommand As DelegateCommand
-        Public Property AutoUpdateCommand() As DelegateCommand
+        Private _AutoUpdateCommand As DelegateCommand, _ManualUpdateCommand As DelegateCommand, _ForceUpdateManualUpdateCommand As DelegateCommand, _CommandWithParameter As DelegateCommand(Of String)
+
+        Public Property AutoUpdateCommand As DelegateCommand
             Get
-                Return privateAutoUpdateCommand
+                Return _AutoUpdateCommand
             End Get
+
             Private Set(ByVal value As DelegateCommand)
-                privateAutoUpdateCommand = value
+                _AutoUpdateCommand = value
             End Set
         End Property
-        Public Property IsAutoUpdateCommandEnabled() As Boolean
+
+        Public Property IsAutoUpdateCommandEnabled As Boolean
             Get
-                Return GetProperty(Function() IsAutoUpdateCommandEnabled)
+                Return GetProperty(Function() Me.IsAutoUpdateCommandEnabled)
             End Get
+
             Set(ByVal value As Boolean)
                 SetProperty(Function() IsAutoUpdateCommandEnabled, value)
             End Set
         End Property
+
         Private Sub AutoUpdateCommandExecute()
             MessageBox.Show("Hello")
         End Sub
+
         Private Function AutoUpdateCommandCanExecute() As Boolean
             Return IsAutoUpdateCommandEnabled
         End Function
 
-        Private privateManualUpdateCommand As DelegateCommand
-        Public Property ManualUpdateCommand() As DelegateCommand
+        Public Property ManualUpdateCommand As DelegateCommand
             Get
-                Return privateManualUpdateCommand
+                Return _ManualUpdateCommand
             End Get
+
             Private Set(ByVal value As DelegateCommand)
-                privateManualUpdateCommand = value
+                _ManualUpdateCommand = value
             End Set
         End Property
-        Public Property IsManualUpdateCommandEnabled() As Boolean
+
+        Public Property IsManualUpdateCommandEnabled As Boolean
             Get
-                Return GetProperty(Function() IsManualUpdateCommandEnabled)
+                Return GetProperty(Function() Me.IsManualUpdateCommandEnabled)
             End Get
+
             Set(ByVal value As Boolean)
                 SetProperty(Function() IsManualUpdateCommandEnabled, value)
             End Set
         End Property
+
         Private Sub ManualUpdateCommandExecute()
             MessageBox.Show("Hello")
         End Sub
+
         Private Function ManualUpdateCommandCanExecute() As Boolean
             Return IsManualUpdateCommandEnabled
         End Function
-        Private privateForceUpdateManualUpdateCommand As DelegateCommand
-        Public Property ForceUpdateManualUpdateCommand() As DelegateCommand
+
+        Public Property ForceUpdateManualUpdateCommand As DelegateCommand
             Get
-                Return privateForceUpdateManualUpdateCommand
+                Return _ForceUpdateManualUpdateCommand
             End Get
+
             Private Set(ByVal value As DelegateCommand)
-                privateForceUpdateManualUpdateCommand = value
+                _ForceUpdateManualUpdateCommand = value
             End Set
         End Property
+
         Private Sub ForceUpdateManualUpdateCommandExecute()
             ManualUpdateCommand.RaiseCanExecuteChanged()
         End Sub
 
-        Private privateCommandWithParameter As DelegateCommand(Of String)
-        Public Property CommandWithParameter() As DelegateCommand(Of String)
+        Public Property CommandWithParameter As DelegateCommand(Of String)
             Get
-                Return privateCommandWithParameter
+                Return _CommandWithParameter
             End Get
+
             Private Set(ByVal value As DelegateCommand(Of String))
-                privateCommandWithParameter = value
+                _CommandWithParameter = value
             End Set
         End Property
+
         Private Sub CommandWithParameterExecute(ByVal parameter As String)
             MessageBox.Show(parameter)
         End Sub
+
         Private Function CommandWithParameterCanExecute(ByVal parameter As String) As Boolean
             Return Not String.IsNullOrEmpty(parameter)
         End Function
